9c3e32d8d88af9206e1ea25ca59afa893cbd5fb9
Jeremy <jeremy51b5@protonmail.com>
2023-04-17 20:17:41 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ bash ./build/install_packages.sh && \
 # Get dotfiles.
 bash ./build/get_dotfiles.sh && \
 
+echo "Finishing with oh-my-zsh installation...";
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+
 echo "Enviroment setup complete. Press Ctrl+R to reload i3.";
 
 exit 0;
